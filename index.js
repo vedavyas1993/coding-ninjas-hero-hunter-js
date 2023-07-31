ba16535a09de8f2994730119b55bcb3272186029
vedavyas1993 <76591323+vedavyas1993@users.noreply.github.com>
2023-07-31 22:12:21 +0530

    test

--- index.js
+++ index.js
@@ -103,7 +103,7 @@ async function getCharacters(ofst) {
   // );
   let response;
   await fetch(
-    `http://gateway.marvel.com/v1/public/characters?ts=${ts}&apikey=d2f97728c6c92cd4cf6452b07f556304&hash=${hash}&limit=${limit}&offset=${ofst}`
+    `https://gateway.marvel.com/v1/public/characters?ts=${ts}&apikey=d2f97728c6c92cd4cf6452b07f556304&hash=${hash}&limit=${limit}&offset=${ofst}`
   )
     .then((response) => {
       if (!response.ok) {
