d4b3e9fec4535d90f7aff77f7ed8de17d2953b69
vedavyas1993 <76591323+vedavyas1993@users.noreply.github.com>
2023-08-09 19:19:43 +0530

    fixed pages issue

--- index.js
+++ index.js
@@ -1,57 +1,11 @@
 import { tsAndHash } from "./utils/hashGenerator.js";
 import { emptyHeart, fillHeart } from "./utils/heartIcons.js";
-// function for timestamp and hash generation
-// function tsAndHash() {
-//   let ts = Date.now();
-//   let hash = CryptoJS.MD5(
-//     ts +
-//       "31ae0263348c45192241c66356f2a13e35a9c47b" +
-//       "d2f97728c6c92cd4cf6452b07f556304"
-//   ).toString();
-//   return { ts, hash };
-// }
 
 let limit = 50;
 let offset = 0;
 let pages;
 let results;
 let favouriteHeros = JSON.parse(localStorage.getItem("favouriteHeros"));
-
-// empty like icon
-// function emptyHeart(id) {
-//   return `
-//     <svg
-//       xmlns="http://www.w3.org/2000/svg"
-//       width="22"
-//       height="22"
-//       fill="red"
-//       data-id=${id}
-//       class="bi bi-heart likes"
-//       viewBox="0 0 16 16"
-//     >
-//       <path data-id=${id} class="likes" d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z" />
-//     </svg>
-//   `;
-// }
-// // filled like icon
-// function fillHeart(id) {
-//   return `<svg
-//           xmlns="http://www.w3.org/2000/svg"
-//           width="22"
-//           height="22"
-//           fill="red"
-//           class="bi bi-heart-fill likes"
-//           viewBox="0 0 16 16"
-//           data-id=${id}
-// >
-//           <path
-//                       data-id=${id}
-//                       class="likes"
-//             fill-rule="evenodd"
-//             d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"
-//           />
-//         </svg>`;
-// }
 
 // like handler while loading
 function handleLikeAndDislike(hero) {
@@ -96,6 +50,7 @@ function addHeroToDOM(hero, cardsContainer) {
 async function getCharacters(ofst) {
   const { ts, hash } = tsAndHash();
   let pagesList = document.getElementsByClassName("pages-list")[0];
+  pagesList.innerHTML = "";
   let cardsContainer = document.getElementById("cards");
   cardsContainer.innerHTML = `<div id="loader-container">
   <span id="loader"></span>
@@ -103,26 +58,6 @@ async function getCharacters(ofst) {
   const response = await axios.get(
     `https://gateway.marvel.com/v1/public/characters?ts=${ts}&apikey=d2f97728c6c92cd4cf6452b07f556304&hash=${hash}&limit=${limit}&offset=${ofst}`
   );
-  // let response;
-  // await fetch(
-  //   `https://gateway.marvel.com/v1/public/characters?ts=${ts}&apikey=d2f97728c6c92cd4cf6452b07f556304&hash=${hash}&limit=${limit}&offset=${ofst}`
-  // )
-  //   .then((response) => {
-  //     if (!response.ok) {
-  //       throw new Error("Network response was not ok");
-  //     }
-  //     return response.json();
-  //   })
-  //   .then((data) => {
-  //     // Process the data here
-  //     console.log(data);
-  //     response = data;
-  //   })
-  //   .catch((error) => {
-  //     // Handle errors here
-  //     console.error("Error fetching data:", error);
-  //   });
-  // results is items
   const { count, total } = response.data.data;
   results = response.data.data.results;
   pages = Math.ceil(total / limit);
@@ -136,6 +71,7 @@ async function getCharacters(ofst) {
     addHeroToDOM(element, cardsContainer);
   });
 }
+
 if (
   window.location.pathname == "/index.html" ||
   window.location.pathname == "/"
